952675c5d4c6beede2ff5bd869a14b723c4faf4e
Kritthik Rajeev Nair <kritthik2609@gmail.com>
2025-12-17 23:56:10 +0530

    Add files via upload

---
+++ CustomInput.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout<<"Enter the value of first number: ";
+    cin>>num1;
+
+    cout<<"Enter the value of second number: ";
+    cin>>num2;
+
+    cout<<"Sum of the two numbers is: "<< num1 + num2;
+
+    return 0;
+}
